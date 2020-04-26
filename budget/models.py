2a70b649d9d70d065f85dec798461fa7cecf6d08
wzsamuels <wzsamuels@gmail.com>
2020-04-26 15:44:17 -0700

    Added date and type fields to the Transaction model.

--- budget/models.py
+++ budget/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 class Transaction(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=20)
+    date = models.DateField(default=date.today)
+    WITHDRAWAL = 'WD'
+    DEPOSIT = "DP"
+    TYPE_CHOICES = [ 
+    	(WITHDRAWAL, 'Withdrawal'),
+    	(DEPOSIT, 'Deposit'),
+	]
+    transType = models.CharField(
+    	max_length=2,
+    	choices=TYPE_CHOICES,
+    	default=WITHDRAWAL
+    )
     
