1ce72b0ef9787ed38d0b47a65a2ca275027771b7
William Samuels <wzsamuels@gmail.com>
2020-05-02 00:04:21 +0000

    Overhauled popup for book and author forms. Top navbar now supports dropdown links.

--- books/forms.py
+++ books/forms.py
@@ -3,15 +3,18 @@ from django import forms
 from books.models import Author, Book
 
 class AuthorForm(ModelForm):
-	class Meta:
-		model = Author
-		fields = ['last_name', 'first_name']
+    class Meta:
+        model = Author
+        fields = ['last_name', 'first_name']
 
 class BookForm(ModelForm):
-	#auto_id=False
-	class Meta:
-		model = Book
-		fields = ['title', 'authors'] #'__all__'
+    class Meta:
+        model = Book
+        fields = ['title', 'authors'] #'__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['authors'].widget.attrs.update({'class' : 'selector'})
 #class BookForm(forms.Form):
  #   title = forms.CharField(label='title', max_length=100)
   #  authors = forms.ModelMultipleChoiceField(queryset=Author.objects.all())
