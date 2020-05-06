021a0dd0ef87aa8874fb464bb0b8f7517fd93b39
William Samuels <wzsamuels@gmail.com>
2020-05-06 23:50:57 +0000

    Cleaned up CSS and JS for the Books apps.

--- static/books/book_list.js
+++ static/books/book_list.js
@@ -1,7 +1,24 @@
 $(function() {
+    $('#AddBookOpen').click(function(){
+        $('#BookForm').show();
+    });
+    
+    $('#AddBookClose').click(function(){
+        $('#BookForm').hide();
+    });
+
+    $('#AddAuthorOpen').click(function(){
+        $('#AuthorForm').show();
+    });
+
+    $('#AddAuthorClose').click(function(){
+        $('#AuthorForm').hide();
+    });
+    
     $('#UpdateAuthorOpen').click(function(){
         $('#AuthorUpdate').show();
     });
+    
     $('#UpdateAuthorClose').click(function(){
         $('#AuthorUpdate').hide();
     });
@@ -21,57 +38,50 @@ $(function() {
     $('#AuthorSort').click(function(){
         $('#SortByAuthor').show();
         $('#SortByTitle').hide();
+    });  
+
+    function bookSearch() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("BookSearchInput");
+        if(input) {
+            filter = input.value.toUpperCase();
+        }
+        ul = document.getElementById("bookList");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            if( document.getElementById("SearchByTitle").checked ) {
+                a = li[i].getElementsByTagName("a")[0];
+        }
+            else {
+                a = li[i].getElementsByTagName("a")[1];
+        }
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
+    $('#SearchByTitle').click(bookSearch);
+    $('#SearchByAuthor').click(bookSearch);
+    
+    $('#BookSearchInput').keyup(bookSearch);
+    
+    // When the user clicks anywhere outside of the modal, close it    
+    $(window).click(function(event) {
+        let bookForm = document.getElementById("BookForm");
+        let authorForm = document.getElementById("AuthorForm");
+        if (event.target == authorForm) {
+            authorForm.style.display = "none";
+        }
+        if (event.target == bookForm) {
+            bookForm.style.display = "none";
+        }
     });
+
+    $( ".datepicker" ).datepicker();
+
+    $('#SortByAuthor').hide();
 });
-
-function openBookForm() {
-    document.getElementById("BookForm").style.display = "block";
-}
-
-function closeBookForm() {
-    document.getElementById("BookForm").style.display = "none";
-}
-
-function openAuthorForm() {
-    document.getElementById("AuthorForm").style.display = "block";
-}
-
-function closeAuthorForm() {
-    document.getElementById("AuthorForm").style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    let bookForm = document.getElementById("BookForm");
-    let authorForm = document.getElementById("AuthorForm");
-    if (event.target == authorForm) {
-        authorForm.style.display = "none";
-    }
-    if (event.target == bookForm) {
-        bookForm.style.display = "none";
-    }
-}
-
-function bookSearch() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("bookSearchInput");
-    if(input) {
-        filter = input.value.toUpperCase();
-    }
-    ul = document.getElementById("bookList");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        if( document.getElementById("SearchByTitle").checked ) {
-            a = li[i].getElementsByTagName("a")[0];
-        }
-        else {
-            a = li[i].getElementsByTagName("a")[1];
-        }
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
