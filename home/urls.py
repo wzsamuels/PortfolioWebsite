b60739fbafc4c2675c68c7f6e2c47174f78f89d3
wzsamuels <wzsamuels@gmail.com>
2021-04-21 19:58:27 -0400

    Added PyCharm project files.

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.home_index, name = 'home_index'),
-    path('c/', views.c_index, name = 'c_index'),
+    path('', views.home_index, name='home_index'),
+    path('c/', views.c_index, name='c_index'),
     path('college/', views.college_index, name='college_index'),
 ]
