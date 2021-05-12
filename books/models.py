9ff37d17e65c822679c185be9f670565d3d56b90
wzsamuels <wzsamuels@gmail.com>
2021-05-12 10:05:02 -0400

    Books with duplicate titles now supported. Unique slugs generated for duplicate titles.

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils.text import slugify
+import re
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.contrib.auth.models import User
 
@@ -15,7 +16,8 @@ class Collection(models.Model):
 class Author(models.Model):
     last_name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
-    slug = models.SlugField(null=False, unique=True)
+    # slug = models.SlugField(null=False, unique=True)
+    slug = models.SlugField(blank=True);
 
     class Meta:
         ordering = ["last_name"]
@@ -45,14 +47,96 @@ class Book(models.Model):
     @property
     def sort_by_title(self):
         return self.title
-        
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('books:book_detail', kwargs={'slug': self.slug})
 
+    """
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+    """
+
+    def save(self, **kwargs):
+        slug_str = "%s" % self.title
+        unique_slugify(self, slug_str)
+        super().save(**kwargs)
+
+
+"""
+Taken from https://djangosnippets.org/snippets/690/
+"""
+
+
+def unique_slugify(instance, value, slug_field_name='slug', queryset=None,
+                   slug_separator='-'):
+    """
+    Calculates and stores a unique slug of ``value`` for an instance.
+
+    ``slug_field_name`` should be a string matching the name of the field to
+    store the slug in (and the field to check against for uniqueness).
+
+    ``queryset`` usually doesn't need to be explicitly provided - it'll default
+    to using the ``.all()`` queryset from the model's default manager.
+    """
+    slug_field = instance._meta.get_field(slug_field_name)
+
+    slug = getattr(instance, slug_field.attname)
+    slug_len = slug_field.max_length
+
+    # Sort out the initial slug, limiting its length if necessary.
+    slug = slugify(value)
+    if slug_len:
+        slug = slug[:slug_len]
+    slug = _slug_strip(slug, slug_separator)
+    original_slug = slug
+
+    # Create the queryset if one wasn't explicitly provided and exclude the
+    # current instance from the queryset.
+    if queryset is None:
+        queryset = instance.__class__._default_manager.all()
+    if instance.pk:
+        queryset = queryset.exclude(pk=instance.pk)
+
+    # Find a unique slug. If one matches, at '-2' to the end and try again
+    # (then '-3', etc).
+    next_number = 2
+    while not slug or queryset.filter(**{slug_field_name: slug}):
+        slug = original_slug
+        end = '%s%s' % (slug_separator, next_number)
+        if slug_len and len(slug) + len(end) > slug_len:
+            slug = slug[:slug_len - len(end)]
+            slug = _slug_strip(slug, slug_separator)
+        slug = '%s%s' % (slug, end)
+        next_number += 1
+
+    setattr(instance, slug_field.attname, slug)
+
+
+def _slug_strip(value, separator='-'):
+    """
+    Cleans up a slug by removing slug separator characters that occur at the
+    beginning or end of a slug.
+
+    If an alternate separator is used, it will also replace any instances of
+    the default '-' separator with the new separator.
+    """
+    separator = separator or ''
+    if separator == '-' or not separator:
+        re_sep = '-'
+    else:
+        re_sep = '(?:-|%s)' % re.escape(separator)
+    # Remove multiple instances and if an alternate separator is provided,
+    # replace the default '-' separator.
+    if separator != re_sep:
+        value = re.sub('%s+' % re_sep, separator, value)
+    # Remove separator from the beginning and end of the slug.
+    if separator:
+        if separator != '-':
+            re_sep = re.escape(separator)
+        value = re.sub(r'^%s+|%s+$' % (re_sep, re_sep), '', value)
+    return value
