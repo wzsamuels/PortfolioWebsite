c7b4cba0f49aa38a0442182c8ac59e5d9faeb0db
wzsamuels <wzsamuels@gmail.com>
2021-05-12 13:56:25 -0400

    Added YAM project page and test image to main page.

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home_index, name='home_index'),
     path('c/', views.c_index, name='c_index'),
     path('college/', views.college_index, name='college_index'),
+    path('c_sharp/yam.html', views.yam_view, name='yam_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+
 def home_index(request):
     return render(request, 'home/index.html')
+
 
 def college_index(request):
     return render(request, 'home/college.html')
 
+
 def c_index(request):
     return render(request, 'home/c_index.html')
+
+
+def yam_view(request):
+    return render(request, 'home/yam.html')
