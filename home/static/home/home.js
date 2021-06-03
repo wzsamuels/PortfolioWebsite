1160320358063bdc4710eb4d85460b45126dc954
wzsamuels <wzsamuels@gmail.com>
2021-06-02 20:40:12 -0400

    Removed text from project icons to improve readability on small screens. Added pop ups for the mud client and rescue projects.

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -15,12 +15,36 @@ $(function() {
         $('#TicketPopUp').hide("fast");
     });
 
+    $('#MudGridItem').click(function () {
+        $('#MudPopUp').show("fast");
+    });
+
+    $('#MudPopUpClose').click(function () {
+        $('#MudPopUp').hide("fast");
+    });
+
+     $('#RescueGridItem').click(function () {
+        $('#RescuePopUp').show("fast");
+    });
+
+    $('#RescuePopUpClose').click(function () {
+        $('#RescuePopUp').hide("fast");
+    });
 
     // When the user clicks anywhere outside of the modal, close it
     $(window).click(function(event) {
-        let book = document.getElementById("BookPopUp");
-        if (event.target == book) {
+        //let book = document.getElementById("BookPopUp");
+        if (event.target == document.getElementById("BookPopUp")) {
             $('#BookPopUp').hide("fast");
+        }
+        else if (event.target == document.getElementById("TicketPopUp")) {
+            $('#TicketPopUp').hide("fast");
+        }
+        else if (event.target == document.getElementById("MudPopUp")) {
+            $('#MudPopUp').hide("fast");
+        }
+        else if (event.target == document.getElementById("RescuePopUp")) {
+            $('#RescuePopUp').hide("fast");
         }
     });
 });
