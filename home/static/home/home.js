1d72b0d612ddbeb726a70c93610838aa10b332dd
Zach Samuels <wzsamuels@gmail.com>
2021-06-07 19:16:55 -0400

    Made all pop up screen shots into a slideshow. Slide show next/previous buttons are now responsive on screen resize.

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -49,23 +49,22 @@ $(function() {
     });
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1];
+let slideId = ["slideshow-slide-mud", "slideshow-slide-ticket", "slideshow-slide-rescue"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
   }
-
-  slides[slideIndex-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block";
 }
