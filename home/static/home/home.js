991c4ccd45221511bc7f72ce8c7023273cb1ddc1
wzsamuels <wzsamuels@gmail.com>
2021-06-07 20:23:31 -0400

    Fixed bug with displaying multiple slideshows on the same page.

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -49,18 +49,19 @@ $(function() {
     });
 });
 
-let slideIndex = [1,1];
-let slideId = ["slideshow-slide-mud", "slideshow-slide-ticket", "slideshow-slide-rescue"]
+var slideIndex = [1,1,1];
+var slideId = ["slideshow-slide-mud", "slideshow-slide-ticket", "slideshow-slide-rescue"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
 function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
